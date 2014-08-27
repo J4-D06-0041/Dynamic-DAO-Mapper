87d44ab099f5ad3b389119c6d6d033a31dab0095
J4-D06-0041 <intex.jero@gmail.com>
2014-08-27 16:17:18 +0800

    First web mvc library

---
+++ LibTest/src/main/java/com/springpractice/libtest/NewMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.springpractice.libtest;
+
+import com.springpractice.testapp.NewClass;
+
+/**
+ *
+ * @author Jody
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewClass x = new NewClass();
+        System.out.println(x.greet());
+    }
+    
+}
